7888988688f7aafa67e72d773760cefa280cce95
bobd50 <galavu10@gmail.com>
2026-04-01 20:44:34 +0300

    Trigger

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,7 @@ import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
 import { loginSchema } from '@/lib/schemas/auth';
 
+
 export default {
   providers: [
     GitHub,
